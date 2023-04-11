a1be54100fd9e53e9f2048f9e141837c32c1410e
Divyesh Vishwakarma <divyesh1099@gmail.com>
2023-04-11 18:03:51 +0530

    Add Product Details UI

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Bulky.Models;
+﻿using Bulky.DataAccess.Repository;
+using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +9,27 @@ namespace BulkyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ProductRepository _productRepository;
+        private readonly CategoryRepository _categoryRepository;
+        public HomeController(ILogger<HomeController> logger, ProductRepository productRepository, CategoryRepository categoryRepository)
         {
             _logger = logger;
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _productRepository.GetAll(includeProperties: "Category");
+
+            return View(productList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product Product = _productRepository.Get(u=>u.Id == id, includeProperties: "Category");
+
+            return View(Product);
         }
 
         public IActionResult Privacy()
